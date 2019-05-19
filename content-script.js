b8048500a0710674b3c1eecb9897cb31d05a10ee
Malik Ammar Faisal <30975614+ammarbinfaisal@users.noreply.github.com>
2019-05-19 22:54:15 +0530

    first commit

---
+++ content-script.js
@@ -0,0 +1,26 @@
+const eventListener = event => {
+    if (event.which === 192 && event.ctrlKey && event.shiftKey) {
+        observe();
+        window.removeEventListener('keydown', eventListener);
+    }
+}
+
+window.addEventListener('keydown', eventListener);
+
+const alarm = new Audio(chrome.runtime.getURL('alarm.mp3'));
+
+function observe() {
+    console.log('going to wait for the target is online');
+
+    setInterval(() => {
+        const $info = document.getElementsByClassName('O90ur')[0];
+        if ($info) {
+            if ($info.textContent.trim() === 'online') {
+                alarm.play();
+                console.log("get online man");
+            } else {
+                alarm.pause();
+            }
+        }
+    }, 3000);
+}
